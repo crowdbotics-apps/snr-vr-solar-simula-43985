1481c5b5ca482a6ac0331487daed6d18b308a227
Crowdbotics <team@crowdbotics.com>
2023-10-17 16:05:19 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "snr-vr-solar-simula-43985",
+  url: "https://snr-vr-solar-simula-43985.botics.co",
+  api: "https://snr-vr-solar-simula-43985.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
